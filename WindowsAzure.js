7e7e096d7bd7bd9312ccccec9ff46153a1d86261
Faisal Rahman <md.faisal.rahman@gmail.com>
2014-03-26 21:01:53 -0400

    some prototype methids are being added

--- WindowsAzure.js
+++ WindowsAzure.js
@@ -9,7 +9,7 @@ function MobileServiceTable(tableName, client) {
 }
 function MobileServiceClient(applicationUrl, applicationKey) {        
             this.applicationUrl = applicationUrl;
-            this.applicationKey = applicationKey || null;
+            this.applicationKey = applicationKey;
         
             this.currentUser = null;
             this._serviceFilter = null;
@@ -18,6 +18,16 @@ function MobileServiceClient(applicationUrl, applicationKey) {
                 return new MobileServiceTable(tableName, this);
             };
 }
+MobileServiceClient.prototype.login = function (provider, token, useSingleSignOn, callback) {
+        this._login.login(provider, token, callback);
+};
+MobileServiceClient.prototype.logout = function () {
+    this.currentUser = null;
+};
+
+MobileServiceClient.prototype.invokeApi =  function (apiName, options, callback) {   
+};
+        
 exports.MobileServiceTable = MobileServiceTable;
 exports.MobileServiceClient = MobileServiceClient; 
 exports.version = '1.0.0';
